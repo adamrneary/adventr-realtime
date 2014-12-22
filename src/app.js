8dcad92260b31c52165d45d38b96460d1341c006
Adam Neary <adam@neary.com>
2014-12-22 10:41:29 -0500

    remove jsx from app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,20 @@
-/**
- * @jsx React.DOM
- */
-'use strict';
-
 var React = require('react');
 var Firebase = require('firebase');
 var ReactFireMixin = require('reactfire');
+var OdometerComponent = require('./react-odometer-component')
 
 // https://github.com/facebook/react-devtools
 window.React = React;
 
 var firebaseApp = "https://luminous-heat-2841.firebaseio.com/";
 var projectId = location.hash.substring(1);
+if (projectId === '') projectId = 14;
 
 var ViewCount = React.createClass({
   mixins: [ReactFireMixin],
 
   getInitialState: function() {
-    return {data: 0};
+    return {data: {name: '…loading…', events: {'Video View': 10000}}};
   },
 
   componentWillMount: function() {
@@ -27,30 +24,15 @@ var ViewCount = React.createClass({
 
   render: function() {
     return (
-      <div id="viewCount">
-        <h3>{this.state.data.name}</h3>
-        <OdometerComponent value={this.state.data.viewCount} />
-      </div>
+      React.DOM.div({id: "viewCount"}, [
+        React.DOM.h3(null, this.state.data.name),
+        React.createElement(OdometerComponent, {value: this.state.data.events['Video View']})
+      ])
     );
   }
 });
 
-var OdometerComponent = React.createClass({
-  componentDidMount: function(){
-     this.odometer = new Odometer({
-      el: this.getDOMNode(),
-      value: this.props.value
-    });
-  },
-  componentDidUpdate: function() {
-   this.odometer.update(this.props.value)
-  },
-  render: function() {
-    return React.DOM.div()
-  }
-})
-
 React.renderComponent(
-  <ViewCount projectId={projectId}/>,
+  React.createElement(ViewCount, {projectId: projectId}),
   document.getElementById('content')
 );
